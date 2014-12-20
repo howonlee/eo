a363ff873cba63cd8e2f062441e9333d57efa1d8
Howon Lee <howon@howonlee.com>
2014-12-19 18:59:55 -0800

    some work on the reading

--- sat_eo.py
+++ sat_eo.py
@@ -1,15 +1,12 @@
 
-def random_assignment(number of things):
-    pass
-
-def unsat_clauses(things):
-    pass
-
-def flip(thing):
-    pass
-
-def optimize(thing thing thing):
-    pass
-
 if __name__ == "__main__":
-    read the first file
+    with open("frb30-15-cnf/frb30-15-1.cnf", "r") as f:
+        for line in f:
+            if line[0] == "c":
+                continue
+            if line[0] == "p":
+                p, cnf, num_vars, num_clauses = line.split()
+                num_vars = int(num_vars)
+                num_clauses = int(num_clauses)
+            clause = line.split()
+            print clause
