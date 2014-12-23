ee31145769e94d790a5f72c58c625f0924fdcc40
Howon Lee <howon@howonlee.com>
2014-12-23 13:20:25 -0800

    something is working, I just need to debug, that's improvisable

--- sat_eo.py
+++ sat_eo.py
@@ -1,6 +1,8 @@
 import collections
+import operator
 import numpy as np
 import numpy.random as npr
+import random
 
 def read_file(filename):
     pos_constraints = collections.defaultdict(set)
@@ -38,19 +40,38 @@ def random_init(num_vars):
     return npr.random(num_vars) > 0.5
 
 def calc_local_fitness(assignment, pos_constraints, neg_constraints):
-    fitness = np.zeros_like(assignment)
+    fitness = np.zeros_like(assignment, dtype=np.int32)
     for x in xrange(assignment.size):
         for y in pos_constraints[x]:
-            pass ############
+            if assignment[x-1] == assignment[y-1]:
+                fitness[x] += 1
         for y in neg_constraints[x]:
-            pass ############
+            if assignment[x-1] != assignment[y-1]:
+                fitness[x] += 1
     return fitness
 
-def flip_eo(fitness, assignment, tau=1.5):
-    pass ############
-    return assignment
+def get_kth_highest_arg(ls, k):
+    return sorted(enumerate(ls), key=operator.itemgetter(1), reverse=True)[k][0]
+
+def flip_eo(fitness, soln, tau=1.5):
+    k = len(soln)
+    while k > len(soln)-1:
+        k = int(np.random.pareto(tau))
+    worst_city = get_kth_highest_arg(fitness, k)
+    new_soln = soln.copy() #deep copy
+    new_soln[worst_city] = not new_soln[worst_city]
+    return new_soln
 
 if __name__ == "__main__":
+    npr.seed(1337)
+    random.seed(1337)
     pos_constraints, neg_constraints, num_vars, num_clauses = read_file("frb30-15-cnf/frb30-15-1.cnf")
     #print "file read..."
-    print random_init(num_vars)
+    init_soln = random_init(num_vars)
+    soln = init_soln
+    fitness = np.zeros_like(soln)
+    for i in xrange(100):
+        fitness = calc_local_fitness(soln, pos_constraints, neg_constraints)
+        soln = flip_eo(fitness, soln)
+    print "soln: ", soln
+    print "fitness: ", fitness
