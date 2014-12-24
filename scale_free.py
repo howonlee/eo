33cd2811480251cf0abb62512c1c5b95f6b78812
Howon Lee <howon@howonlee.com>
2014-12-23 21:08:08 -0800

    scale freedom... later

---
+++ scale_free.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy as sci
+
+"""
+Scale-free search
+"""
+
+"""
+Try on Rosenbrock's function
+f(x,y) = (a-x)^2 + b(y-x^2)^2
+"""
+
+if __name__ == "__main__":
+    pass
