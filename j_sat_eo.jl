d7225870aa0c937c4c47ae268397f7d8ce75d9f8
Howon Lee <howon@howonlee.com>
2015-01-10 16:02:30 -0800

    translation is fun

--- j_sat_eo.jl
+++ j_sat_eo.jl
@@ -2,8 +2,8 @@ using PyPlot
 using Distributions
 
 function parse_cnf(filename):
-  pos_constraints = (String => Any)[]
-  neg_constraints = (String => Any)[]
+  pos_constraints = (Int => Int)[]
+  neg_constraints = (Int => Int)[]
   num_vars = -1
   num_clauses = -1
   f = open(filename)
@@ -19,6 +19,7 @@ function parse_cnf(filename):
       continue
     end
     clause = map(parseint, line.split())
+    set_clause = Set(clause)
     for var in clause
       if var > 0
         pos_constraints[var].update(set(clause)) ####
@@ -28,20 +29,22 @@ function parse_cnf(filename):
       end
     end
   end
-  for key, val in pos_constraints.iteritems() ####
-    if key in val ###
-      val.remove(key) ###
+  for key in keys(pos_constraints)
+    val = get(pos_constraints, key)
+    if haskey(val, key)
+      delete!(val, key)
     end
-    if 0 in val ###
-      val.remove(0) ####
+    if haskey(val, 0)
+      delete!(val, 0)
     end
   end
-  for key, val in neg_constraints.iteritems() ####
-    if key in val ###
-      val.remove(key) ####
+  for key in neg_constraints
+    val = get(pos_constraints, key)
+    if haskey(val, key)
+      delete!(val, key)
     end
-    if 0 in val ####
-      val.remove(0) ####
+    if haskey(val, 0)
+      delete!(val, 0)
     end
   end
   close(f)
@@ -51,3 +54,7 @@ end
 ### main program begins
 
 pos_constraints, neg_constraints, num_vars, num_clauses = parse_cnf("frb30-15-cnf/frb30-15-1.cnf")
+print(pos_constraints)
+print(neg_constraints)
+print(num_vars)
+print(num_clauses)
